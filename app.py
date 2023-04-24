8bb26a28a9b83858db53c7329aa3eb146a5b0e43
Yusuf <bsse1013@iit.du.ac.bd>
2023-04-24 06:45:35 +0600

    flash removed

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from werkzeug.utils import secure_filename, redirect, send_from_directory
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "static/uploads/"
+app.config["SECRET_KEY"] = "This is not a secret"
 
 
 @app.route('/')
@@ -13,16 +14,11 @@ def upload_file():
 @app.route('/display', methods=['GET', 'POST'])
 def save_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return render_template('content.html', message='No selected file')
 
         filename = secure_filename(file.filename)
         file.save(app.config['UPLOAD_FOLDER'] + filename)
@@ -33,7 +29,8 @@ def save_file():
 
         return render_template('content.html', filename=filename)
 
-    return render_template('content.html', content="ERROR")
+    # return render_template('content.html', message="ERROR")
+    return redirect("/")
 
 
 if __name__ == '__main__':
